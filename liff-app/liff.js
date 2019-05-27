1e0ebe0d4ad4325512b794214516ea2724eb7c9a
Peritec-Nakata <48009385+Peritec-Nakata@users.noreply.github.com>
2019-05-27 09:48:43 +0900

    Update liff.js

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = 'f596c5f4-34e0-4c68-8b41-c737193cf49f'; // LED, Button
+const USER_SERVICE_UUID         = '46f238fb-41b6-4dfe-bea4-5afaecf54f19'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
